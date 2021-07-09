10dd7a85041d92918d78042a027de788e0e1326b
Roland Nemes <nemesroland2@gmail.com>
2021-07-09 20:49:47 +0300

    Autocommit.

--- autostart.sh
+++ autostart.sh
@@ -4,6 +4,6 @@ pgrep -x picom > /dev/null || picom &
 pgrep -x startdunst > /dev/null || startdunst &
 pgrep -x unclutter > /dev/null || unclutter &
 pgrep -x mpd > /dev/null || mpd &
-xset s 180
+xset s 300
 pgrep -x xss-lock > /dev/null || xss-lock -- lockscreen &
 pgrep -x dwmblocks > /dev/null && killall dwmblocks && dwmblocks || dwmblocks &
