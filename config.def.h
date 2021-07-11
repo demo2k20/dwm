0e76ce47152124c343ee6a5a5ac2e4999282c682
Roland Nemes <nemesroland2@gmail.com>
2021-07-11 21:28:08 +0300

    Autocommit.

--- config.def.h
+++ config.def.h
@@ -3,8 +3,8 @@ static const unsigned int borderpx  = 2;         /* border pixel of windows */
 static const unsigned int snap      = 16;        /* snap pixel */
 static const unsigned int gappih    = 8;         /* horiz inner gap between windows */
 static const unsigned int gappiv    = 8;         /* vert inner gap between windows */
-static const unsigned int gappoh    = 32;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 32;        /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 8;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;        /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;         /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;         /* 0 means no bar */
 static const int topbar             = 1;         /* 0 means bottom bar */
@@ -28,7 +28,8 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class       instance    title        tags mask     isfloating   monitor */
-	{ "Alacritty", NULL,       "floatterm", 0,            1,           -1 },
+	{ "tdropterm", NULL,       "floatterm", 0,            1,           -1 },
+	{ "Alacritty", NULL,       "tdropterm", 0,            1,           -1 },
 	{ "discord",   NULL,       NULL,        1 << 8,       0,           -1 },
 	{ "Steam",     NULL,       NULL,        0,            1,           -1 },
 	{ "guvcview",  NULL,       NULL,        0,            1,           -1 },
@@ -86,7 +87,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY,                       XK_Escape,      quit,      {1} }, 
+	{ MODKEY,                       XK_Escape,      quit,      {1} },
 	{ MODKEY|ShiftMask,             XK_Escape,      quit,      {0} },
 };
 
