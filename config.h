04b7a438816ad7b18ae5a76466b7386fbe1f1c96
Roland Nemes <nemesroland2@gmail.com>
2021-07-12 15:52:16 +0300

    Autocommit.

--- config.h
+++ config.h
@@ -28,8 +28,8 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class       instance    title        tags mask     isfloating   monitor */
-	{ "tdropterm", NULL,       "floatterm", 0,            1,           -1 },
-	{ "Alacritty", NULL,       "tdropterm", 0,            1,           -1 },
+	{ "Alacritty", NULL,       "spterm",    0,            1,           -1 },
+	{ "Alacritty", NULL,       "floatterm", 0,            1,           -1 },
 	{ "discord",   NULL,       NULL,        1 << 8,       0,           -1 },
 	{ "Steam",     NULL,       NULL,        0,            1,           -1 },
 	{ "guvcview",  NULL,       NULL,        0,            1,           -1 },
