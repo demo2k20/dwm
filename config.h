15abf42c47224b957b507505bc9a358c2d0cb014
Roland Nemes <nemesroland2@gmail.com>
2021-07-25 20:10:41 +0300

    Autocommit.

--- config.h
+++ config.h
@@ -3,8 +3,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const unsigned int gappih    = 8;        /* horiz inner gap between windows */
 static const unsigned int gappiv    = 8;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 8;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;        /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -16,7 +16,7 @@ static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
-static const unsigned int baralpha = 200;
+static const unsigned int baralpha = 255;
 static const unsigned int borderalpha = OPAQUE;
 static char *colors[][3] = {
        /*               fg           bg              border          */
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	/* class       instance    title        tags mask     isfloating   monitor */
 	{ "Alacritty", NULL,       "floatterm", 0,            1,           -1 },
 	{ "Alacritty", NULL,       "spterm",    0,            1,           -1 },
+	{ "Alacritty", NULL,       "bcterm",    0,            1,           -1 },
 	{ "discord",   NULL,       NULL,        1 << 8,       0,           -1 },
 	{ "Element",   NULL,       NULL,        1 << 7,       0,           -1 },
 	{ "Steam",     NULL,       NULL,        0,            1,           -1 },
