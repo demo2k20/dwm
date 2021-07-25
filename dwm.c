32b5e35235eae31c4c092cb75ec53cb8fbacd630
Roland Nemes <nemesroland2@gmail.com>
2021-07-26 02:41:27 +0300

    Autocommit.

--- dwm.c
+++ dwm.c
@@ -1168,12 +1168,12 @@ loadxrdb()
       xrdb = XrmGetStringDatabase(resm);
 
       if (xrdb != NULL) {
-        XRDB_LOAD_COLOR("dwm.normbordercolor", normbordercolor);
-        XRDB_LOAD_COLOR("dwm.normbgcolor", normbgcolor);
-        XRDB_LOAD_COLOR("dwm.normfgcolor", normfgcolor);
-        XRDB_LOAD_COLOR("dwm.selbordercolor", selbordercolor);
-        XRDB_LOAD_COLOR("dwm.selbgcolor", selbgcolor);
-        XRDB_LOAD_COLOR("dwm.selfgcolor", selfgcolor);
+        XRDB_LOAD_COLOR("*.background", normbordercolor);
+        XRDB_LOAD_COLOR("*.background", normbgcolor);
+        XRDB_LOAD_COLOR("*.foreground", normfgcolor);
+        XRDB_LOAD_COLOR("*.color1", selbordercolor);
+        XRDB_LOAD_COLOR("*.selbackground", selbgcolor);
+        XRDB_LOAD_COLOR("*.selforeground", selfgcolor);
       }
     }
   }
