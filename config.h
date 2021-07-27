5a7434c3a20a1ff26ff5f0eb68feb159288739be
Roland Nemes <nemesroland2@gmail.com>
2021-07-27 18:34:31 +0300

    Autocommit.

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
-static const unsigned int gappih    = 8;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 8;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 8;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;        /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 4;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 4;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 4;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 4;        /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -37,9 +37,9 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class       instance    title        tags mask     isfloating   monitor */
-	{ "Alacritty", NULL,       "floatterm", 0,            1,           -1 },
-	{ "Alacritty", NULL,       "spterm",    0,            1,           -1 },
-	{ "Alacritty", NULL,       "bcterm",    0,            1,           -1 },
+	{ "St",        NULL,       "floatterm", 0,            1,           -1 },
+	{ "St",        NULL,       "spterm",    0,            1,           -1 },
+	{ "St",        NULL,       "bcterm",    0,            1,           -1 },
 	{ "discord",   NULL,       NULL,        1 << 8,       0,           -1 },
 	{ "Element",   NULL,       NULL,        1 << 7,       0,           -1 },
 	{ "Steam",     NULL,       NULL,        0,            1,           -1 },
@@ -83,7 +83,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_a,      zoom,           {0} },
     { MODKEY,                       XK_s,      view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+   	{ MODKEY,                       XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_e,      cyclelayout,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_e,      cyclelayout,    {.i = -1 } },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
