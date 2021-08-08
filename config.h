abe88bf7f3ad0ded8f714262d689efc61b332d66
Roland Nemes <nemesroland2@gmail.com>
2021-08-09 01:32:38 +0300

    Autocommit.

--- config.h
+++ config.h
@@ -3,15 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 8;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 8;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 8;        /* horizontal inner gap between windows */
+static const unsigned int gappiv    = 8;        /* vertical inner gap between windows */
+static const unsigned int gappoh    = 32;       /* horizontal outer gap between windows and screen edge */
+static const unsigned int gappov    = 32;       /* vertical outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 18;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Terminus:size=10" };
+static const char *fonts[]          = { "Terminus:size=8" };
 static char normbgcolor[]           = "#222222";
 static char urgentbgcolor[]         = "#ff0000";
 static char normbordercolor[]       = "#444444";
@@ -28,7 +28,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
 static const Rule rules[] = {
 	/* class       instance    title        tags mask     isfloating   monitor */
@@ -37,9 +37,6 @@ static const Rule rules[] = {
 	{ "discord",   NULL,       NULL,        1 << 8,       0,           -1 },
 	{ "Element",   NULL,       NULL,        1 << 7,       0,           -1 },
 	{ "Steam",     NULL,       NULL,        0,            1,           -1 },
-	{ "guvcview",  NULL,       NULL,        0,            1,           -1 },
-	{ "Guvcview",  NULL,       NULL,        0,            1,           -1 },
-	{ "Guvcview",  NULL,       NULL,        1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
@@ -51,7 +48,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "[M]",      monocle },
+	{ "[M]",      monocle }, /* second entry is monocle */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
@@ -86,7 +83,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    focusmon,       {.i = +1 } }, // Focus next monitor
 	{ MODKEY|ShiftMask,             XK_Tab,    tagmon,         {.i = +1 } }, // Send window to next monitor
 	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } }, // Reload xresources color settings
-	{ MODKEY|ShiftMask,             XK_minus,  togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_minus,  togglegaps,     {0} }, // Toggle all gaps
 //	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
 //	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
 //	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
