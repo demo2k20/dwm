e3f222db9ed8fdde2c616018f644993ff0f1451c
Roland Nemes <nemesroland2@gmail.com>
2021-10-17 18:40:04 +0300

    Autocommit.

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 8;        /* horizontal inner gap between windows */
 static const unsigned int gappiv    = 8;        /* vertical inner gap between windows */
-static const unsigned int gappoh    = 16;       /* horizontal outer gap between windows and screen edge */
-static const unsigned int gappov    = 24;       /* vertical outer gap between windows and screen edge */
+static const unsigned int gappoh    = 8;       /* horizontal outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;       /* vertical outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
