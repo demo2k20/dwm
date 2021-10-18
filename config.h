06b00f1705382e35fed4eeea1ad9067300f3bf7e
Roland Nemes <nemesroland2@gmail.com>
2021-10-18 18:17:41 +0300

    Autocommit.

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 8;        /* horizontal inner gap between windows */
 static const unsigned int gappiv    = 8;        /* vertical inner gap between windows */
-static const unsigned int gappoh    = 8;       /* horizontal outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;       /* vertical outer gap between windows and screen edge */
+static const unsigned int gappoh    = 8;        /* horizontal outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;        /* vertical outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -36,8 +36,8 @@ static const Rule rules[] = {
 	{ NULL,        NULL,       "bcterm",    0,            1,           -1 },
 	{ NULL,        NULL,       "floatterm", 0,            1,           -1 },
 	{ "Steam",     NULL,       NULL,        0,            1,           -1 },
-	{ "discord",   NULL,       NULL,        1 << 8,       0,            1 },
-	{ "Element",   NULL,       NULL,        1 << 7,       0,            1 },
+	{ "discord",   NULL,       NULL,        1 << 8,       0,           -1 },
+	{ "Element",   NULL,       NULL,        1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
